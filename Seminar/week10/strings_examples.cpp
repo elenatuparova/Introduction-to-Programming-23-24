49e259b1e2bc8358340e854331ce0b7475599f7d
Elena Tuparova <elena.tuparova@gmail.com>
2023-12-14 18:15:37 +0200

    Update materials from week 10 (more string fundamentals)

--- Seminar/week10/strings_examples.cpp
+++ Seminar/week10/strings_examples.cpp
@@ -1,37 +1,82 @@
 #include <iostream>
 
 int getLength(const char str[]) {
-	int length = 0;
-	int index = 0;
-	while (str[index] != '\0') {
-		index++;
-		length++;
-	}
-	return length;
+    int length = 0;
+    int index = 0;
+    while (str[index] != '\0') {
+        index++;
+        length++;
+    }
+    return length;
 }
 
-int getLengthWithPointers(const char* pstr) {
-	if (pstr == nullptr) {
-		return 0;
-	}
+int getLengthWithPointers(const char *pstr) {
+    if (pstr == nullptr) {
+        return 0;
+    }
 
-	int length = 0;
-	while (*pstr != '\0') {
-		pstr++;
-		length++;
-	}
-	return length;
+    int length = 0;
+    while (*pstr != '\0') {
+        pstr++;
+        length++;
+    }
+    return length;
+}
+
+int compareStrings(const char *first, const char *second) {
+    if (!first || !second) {
+        return -1;
+    }
+
+    while ((*first) == (*second) && (*first) && (*second)) {
+        first++;
+        second++;
+    }
+    return *first - *second;
+}
+
+int compareStrings2(const char *first, const char *second) {
+    if (!first || !second) {
+        return -1;
+    }
+
+    int index = 0;
+    while (first[index] == second[index] && first[index] && second[index]) {
+        index++;
+    }
+    return first[index] - second[index];
+}
+
+void copyString(const char *source, char *dest) {
+    if (!source || !dest) {
+        return;
+    }
+
+    while (*source) {
+        *dest = *source;
+        dest++;
+        source++;
+    }
+    *dest = '\0';
+}
+
+void concatString(const char *source, char *dest) {
+    if (!source || !dest) {
+        return;
+    }
+    int destLength = getLengthWithPointers(dest);
+    dest += destLength;
+    copyString(source, dest);
 }
 
 int main() {
-	//char str1[3] = { 'F', 'M' };
-	//std::cout << str1 << std::endl;
+    // char str1[3] = { 'F', 'M' };
+    // std::cout << str1 << std::endl;
 
-	char str[] = "Hello!";
-	char* pstr = str;
-	while (*pstr) {
-		std::cout << *pstr;
-		pstr++;
-	}
-
+    char str[] = "Hello!";
+    char *pstr = str;
+    while (*pstr) {
+        std::cout << *pstr;
+        pstr++;
+    }
 }
